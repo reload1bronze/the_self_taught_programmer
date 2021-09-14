010dece9a3ae2d72c2df9119bee2e113af13d880
reload1bronze <nature9th@gmail.com>
2021-09-14 22:27:16 +0900

    프로그래머 첫걸음 도서 학습 중 객체 지향 프로그래밍 이해

--- the_self_taught_programmer/ch_11_paradigm.py
+++ the_self_taught_programmer/ch_11_paradigm.py
@@ -19,3 +19,16 @@ class Circle:
 
 circle = Circle(10)
 circle.area()
+
+
+class Triangle:
+    def __init__(self, length, height):
+        self.length = length
+        self.height = height
+
+    def area(self):
+        print((self.length * self.height) * (1 / 2))
+
+
+triangle = Triangle(10, 10)
+triangle.area()
